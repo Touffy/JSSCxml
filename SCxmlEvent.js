24436869c06ef88ce6a5ae6ff82852cba1aa5625
David Junger <tffy@free.fr>
2012-07-14 12:36:12 +0200

    split Events into another JS file

---
+++ SCxmlEvent.js
@@ -0,0 +1,63 @@
+/*
+Constructors for SCXML events, since the browser's built-in DOM Events
+are not ideally suited for that role.
+*/
+
+// the basic constructor:
+SCxml.Event=function SCxmlEvent(name, type)
+{
+	this.name=name
+	this.timestamp=new Date().getTime()
+	this.type=type||"platform"
+}
+
+SCxml.Event.prototype.toString=function ()
+{ return "SCxmlEvent("+this.name+")" }
+SCxml.Event.prototype.match=function (t)
+// matches transitions and events, e.g. "user.*" matches "user.login"
+// the argument is an actual <transtion> element
+{
+	pattern=t.getAttribute("event").split(".")
+	event=this.name.split(".")
+	if(pattern.length>event.length) return false
+	for(var i=0; i<pattern.length; i++)
+		if(pattern[i]!="*" && pattern[i]!=event[i]) return false
+	return true
+}
+
+
+// sub-constructors for internal, error and external events:
+
+SCxml.InternalEvent=function (name, src)
+{
+	SCxml.Event.call(this, name, "internal")
+	this.srcElement=src||null
+}
+SCxml.InternalEvent.prototype=new SCxml.Event()
+SCxml.InternalEvent.prototype.constructor=SCxml.InternalEvent
+
+
+SCxml.Error=function (name, src, err)
+{
+	SCxml.Event.call(this, name)
+	this.srcElement=src||null
+	this.err=err
+	
+//	throw err
+}
+SCxml.Error.prototype=new SCxml.Event()
+SCxml.Error.prototype.constructor=SCxml.Error
+
+
+SCxml.ExternalEvent=function (name, sendid, origin, origintype,
+	invokeid, data)
+{
+	SCxml.Event.call(this, name, "external")
+	this.sendid=sendid||""
+	this.origin=origin||""
+	this.origintype=origintype||""
+	this.invokeid=invokeid||""
+	this.data=data
+}
+SCxml.ExternalEvent.prototype=new SCxml.Event()
+SCxml.ExternalEvent.prototype.constructor=SCxml.ExternalEvent
