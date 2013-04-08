ad546e84ba9c72c70a6a17d9631f7f17e82f8426
David Junger <tffy@free.fr>
2013-04-08 20:10:13 +0200

    system _variables are now read-only, and we hijack setTimeout etc

--- SCxmlDatamodel.js
+++ SCxmlDatamodel.js
@@ -17,7 +17,12 @@ SCxml.prototype.initIframe=function (data)
 			+ 'function expr(s,el)\n{\n'
 			+ '	try{ with(_jsscxml_predefined_){ return eval(s) } }\n'
 			+ '	catch(e){ _sc.error("execution",el,e) }\n'
-			+ '}'
+			+ '}\n'
+			
+			+ 'function call(f, args)\n{\n'
+			+ '	try{ with(_jsscxml_predefined_){ return f.call(null, args) } }\n'
+			+ '	catch(e){ _sc.error("execution",f,e) }\n'
+			+ '}\n'
 
 			+'\n</script>\n')
 	}
@@ -29,10 +34,18 @@ SCxml.prototype.initIframe=function (data)
 	for(var i in this.datamodel) if(this.datamodel.hasOwnProperty(i))
 		this.datamodel._jsscxml_predefined_[i]=undefined
 	 
-	this.datamodel._sessionid=this.sid
-	this.datamodel._event=undefined
-	this.datamodel._name=""
-	this.datamodel._ioprocessors=SCxml.EventProcessors
+	this.datamodel.__defineGetter__("_sessionid", function(){return this._sc.sid})
+	this.datamodel.__defineSetter__("_sessionid", function(){return this._sc.sid})
+	this.datamodel.__defineGetter__("_event", function(){
+		return this._sc.lastEvent})
+	this.datamodel.__defineSetter__("_event", function(){
+		return this._sc.lastEvent})
+	this.datamodel.__defineGetter__("_name", function(){return this._sc.name})
+	this.datamodel.__defineSetter__("_name", function(){return this._sc.name})
+	this.datamodel.__defineGetter__("_ioprocessors", function(){
+		return SCxml.EventProcessors })
+	this.datamodel.__defineSetter__("_ioprocessors", function(){
+		return SCxml.EventProcessors })
 	this.datamodel._x={}
 	
 	if(data) for(i in data) if(data.hasOwnProperty(i))
@@ -43,7 +56,21 @@ SCxml.prototype.initIframe=function (data)
 	}
 	
 	this.datamodel.document.write('<script>\n'
-		+'function In(state){ return state in _sc.configuration }\n</script>')
+		+'function In(state){ return state in _sc.configuration }\n'
+		+'function setTimeout(){\n'
+		+'	return _sc.timeout(arguments)\n}\n'
+		+'function setInterval(){\n'
+		+'	return _sc.interval(arguments)\n}\n'
+		+'function clearTimeout(t){\n'
+		+'	return t.cancel()\n}\n'
+		+'function clearInterval(t){\n'
+		+'	return t.cancel()\n}\n'
+		+'</script>\n')
+	
+	delete this.datamodel._jsscxml_predefined_.setTimeout
+	delete this.datamodel._jsscxml_predefined_.setInterval
+	delete this.datamodel._jsscxml_predefined_.clearTimeout
+	delete this.datamodel._jsscxml_predefined_.clearInterval
 }
 
 SCxml.prototype.wrapScript=function (script, element)
