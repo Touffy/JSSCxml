073d2f87a5ba48e8a3ba3f2d034e6c6f4a73b4b6
David Junger <tffy@free.fr>
2014-01-16 19:12:02 +0100

    <transition target> mutations will now affect the SC and redraw arrows accordingly

---
+++ SCxmlMutation.js
@@ -0,0 +1,237 @@
+SCxml.prototype.mutationRequiresRestart=function()
+{
+
+}
+
+SCxml.prototype.initObservers=function()
+{
+	this.obs={}
+	for(var i in SCxml.mutations){
+		this.obs[i]=new MutationObserver(SCxml.mutations[i]),
+		this.obs[i].sc=this
+	}
+}
+
+SCxml.mutations={
+	scxml:function (mutations, obs)
+	{
+		for(var i in mutations){
+			var e=mutations[i].target
+			if(mutations[i].type=="attributes")
+			switch(mutations[i].attributeName){
+			case "initial":
+				obs.sc.checkTargets(e.getAttribute("initial"), e)
+				// TODO: draw initial arrow (whenever that is implemented…)
+				break
+			case "name":
+				obs.sc.name = e.getAttribute("name")
+					|| obs.sc.iid || ("session"+obs.sc.sid)
+				break
+			case "binding": with(mutations[i].target){
+				if(hasAttribute("binding")
+				&& getAttribute("binding") != "early"
+				&& getAttribute("binding") != "late")
+					console.warn("binding='"+getAttribute("binding")+"' in"
+					+ obs.sc.name +" is not valid")
+				obs.sc.lateBinding=(getAttribute("binding")=="late")
+				}
+				obs.sc.mutationRequiresRestart()
+			}
+			else if(mutations[i].addedNodes.length){
+				// TODO: check that the new id is not already in use
+				var newChild=mutations[i].addedNodes[0]
+				newChild._JSSCID=obs.sc.lastJSSCID++
+				obs.sc.JSSCID[newChild._JSSCID]=newChild
+				switch(newChild.localName){
+				case "state":
+				case "parallel":
+				case "final":
+					obs.sc.newTarget(newChild)
+					break
+				}
+			}
+		}
+	},
+	state:function (mutations, obs)
+	{
+		for(var i in mutations){
+			var e=mutations[i].target
+			if(mutations[i].type=="attributes")
+			switch(mutations[i].attributeName){
+			case "initial":
+				obs.sc.checkTargets(e.getAttribute("initial"), e)
+				// TODO: draw initial arrow (whenever that is implemented…)
+				break
+			case "id":
+				// TODO: check that the new id is not already in use
+				// TODO: refactor transition targetting this state?
+				obs.sc.renameTarget(mutations[i].oldValue, e)
+			}
+			else if(mutations[i].addedNodes.length){
+				// TODO: check that the new id is not already in use
+				var newChild=mutations[i].addedNodes[0]
+				newChild._JSSCID=obs.sc.lastJSSCID++
+				obs.sc.JSSCID[newChild._JSSCID]=newChild
+				switch(newChild.localName){
+				case "state":
+				case "parallel":
+				case "final":
+				case "history":
+					obs.sc.newTarget(newChild)
+					break
+				case "transition":
+					break
+				}
+			}
+		}
+	},
+	parallel:function (mutations, obs)
+	{
+		for(var i in mutations){
+			var e=mutations[i].target
+			if(mutations[i].type=="attributes"
+			&& mutations[i].attributeName=="id"){
+				// TODO: check that the new id is not already in use
+				// TODO: refactor transition targetting this state?
+				obs.sc.renameTarget(mutations[i].oldValue, e)
+			}
+			else if(mutations[i].addedNodes.length){
+				// TODO: check that the new id is not already in use
+				var newChild=mutations[i].addedNodes[0]
+				newChild._JSSCID=obs.sc.lastJSSCID++
+				obs.sc.JSSCID[newChild._JSSCID]=newChild
+				switch(newChild.localName){
+				case "state":
+				case "parallel":
+				case "final":
+				case "history":
+					obs.sc.newTarget(newChild)
+					break
+				case "transition":
+					break
+				}
+			}
+		}
+	},
+	transition:function (mutations, obs)
+	{
+		for(var i in mutations){
+			var e=mutations[i].target
+			switch(mutations[i].attributeName){
+			case "event":
+			case "cond":
+			case "type":
+			// someday: update the "next step" display
+				break
+			case "target":
+			case "targetexpr":
+				var oldTargets
+				// clear targets' reverse links
+				if(oldTargets=e.targets && obs.sc.resolve(e.targets))
+					for(var j=0, t; t=oldTargets[j]; j++)
+						obs.sc.targets[t.getAttribute("id")].remove(e._JSSCID)
+				var expr=false
+				// maybe recompute target set and reverse
+				if(!e.getAttribute("target")){
+					e.targets=""
+					if(e.ui) obs.sc.view.clearArrows(e.ui)
+					expr=!!e.getAttribute("targetexpr")
+				} else {
+					obs.sc.checkTargets(e.getAttribute("target"), e)
+					if(e.ui) obs.sc.view.drawTransition(e.ui)
+				}
+			}
+		}
+	}
+}
+
+SCxml.prototype.newTarget=function(s)
+{
+	var newId=s.getAttribute("id")
+	if(this.view)
+		s.parentNode.ui.appendChild(this.view.convertNode(s))
+	if(newId in this.missingTargets){
+		for(var i=0, t; t=this.missingTargets[newId][i]; i++){
+			t.targets.add(s._JSSCID)
+			if(t.ui) this.view.drawTransition(t.ui)
+		}
+		this.targets[newId]=this.missingTargets[newId]
+		delete this.missingTargets[newId]
+	}
+}
+
+SCxml.prototype.renameTarget=function(oldId, s)
+{
+	var newId=s.getAttribute("id")
+	if(oldId in this.targets){
+		for(var i=0, t; t=this.targets[oldId][i]; i++){
+			t.targets.remove(s._JSSCID)
+			if(t.ui) this.view.drawTransition(t.ui)
+		}
+		this.missingTargets[oldId]=this.targets[oldId]
+		delete this.targets[oldId]
+	}
+	if(newId in this.missingTargets){
+		for(var i=0, t; t=this.missingTargets[newId][i]; i++){
+			t.targets.add(s._JSSCID)
+			if(t.ui) this.view.drawTransition(t.ui)
+		}
+		this.targets[newId]=this.missingTargets[newId]
+		delete this.missingTargets[newId]
+	}
+}
+
+SCxml.observerOptions={
+	scxml:{
+		childList: true,
+		attributes:true,
+		attributeFilter: ["initial", "name", "binding"]
+	},
+	state:{
+		childList: true,
+		attributes:true,
+		attributeFilter: ["id","initial"],
+		attributeOldValue:true
+	},
+	parallel:{
+		childList: true,
+		attributes:true,
+		attributeFilter: ["id"],
+		attributeOldValue:true
+	},
+	transition:{
+		attributes:true,
+		attributeFilter: ["event", "cond", "target", "targetexpr", "type"]
+	},
+	initial:{childList: true},
+	"final":{
+		childList: true,
+		attributes:true,
+		attributeFilter: ["id"],
+		attributeOldValue:true
+	},
+	onentry:{childList: true},
+	onexit:{childList: true},
+	donedata:{childList: true},
+	history:{
+		childList: true,
+		attributes:true,
+		attributeFilter: ["id","type"],
+		attributeOldValue:true
+	},
+	
+	datamodel:{childList: true},
+	script:{
+		childList: true,
+		attributes:true,
+		attributeFilter: ["src"]
+	},
+	
+	invoke:{
+		childList: true,
+		attributes:true,
+		attributeFilter: ["id","idlocation","type","typeexpr","src","srcexpr",
+			"autoforward"]
+	},
+	finalize:{childList: true}
+}
