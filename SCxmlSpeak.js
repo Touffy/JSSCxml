bc7c41876ab40553cf8349de4adeba5bcfca2695
David Junger <tffy@free.fr>
2014-03-12 17:22:27 +0100

    changed the error event for SpeehSynthesisUtterances to speak.error

--- SCxmlSpeak.js
+++ SCxmlSpeak.js
@@ -16,31 +16,38 @@ exc._support_.speakEvent=function(de, sc){
 
 exc.speak=function(sc, element)
 {
+	var stop=element.hasAttribute("interrupt")
+		|| element.hasAttribute("nomore")
+
 	var text=element.getAttribute("text")
 		||sc.expr(element.getAttribute("expr"), element)
-	if(!text) sc.error("execution",element,
-		new Error('cannot speak an empty string'))
-	var s=new SpeechSynthesisUtterance(text)
-	s.onerror=function(e){sc.error("execution.speak", element, e)}
-
-	s.lang=element.getAttributeNS("http://www.w3.org/XML/1998/namespace", "lang")
-		||element.getAttribute("lang")
-		||sc.expr(element.getAttribute("langexpr"), element)
-		||sc.dom.documentElement.getAttributeNS(
-			"http://www.w3.org/XML/1998/namespace", "lang")
-		||sc.dom.documentElement.getAttribute("lang")
-
-	if((s.voice=sc.expr(element.getAttribute("voice"), element))
-	&& 'voiceURI' in s.voice) s.voiceURI=s.voice.voiceURI
-	s.rate=+sc.expr(element.getAttribute("rate"), element) || 1
-	s.pitch=+sc.expr(element.getAttribute("pitch"), element) || 1
-	s.volume=+sc.expr(element.getAttribute("volume"), element) || 1
+	if(text){
+		var s=new SpeechSynthesisUtterance(text)
 	
-	s.onend=function(e){exc._support_.speakEvent(e, sc)}
-	s.onmark=function(e){exc._support_.speakEvent(e, sc)}
-	s.onboundary=function(e){exc._support_.speakEvent(e, sc)}
-
-	speechSynthesis.speak(s)
+		s.lang=element.getAttributeNS("http://www.w3.org/XML/1998/namespace", "lang")
+			||element.getAttribute("lang")
+			||sc.expr(element.getAttribute("langexpr"), element)
+			||sc.dom.documentElement.getAttributeNS(
+				"http://www.w3.org/XML/1998/namespace", "lang")
+			||sc.dom.documentElement.getAttribute("lang")
+	
+		if((s.voice=sc.expr(element.getAttribute("voice"), element))
+		&& 'voiceURI' in s.voice) s.voiceURI=s.voice.voiceURI
+		s.rate=+sc.expr(element.getAttribute("rate"), element) || 1
+		s.pitch=+sc.expr(element.getAttribute("pitch"), element) || 1
+		s.volume=+sc.expr(element.getAttribute("volume"), element) || 1
+		
+		s.onstart=function(e){exc._support_.speakEvent(e, sc)}
+		s.onend=function(e){exc._support_.speakEvent(e, sc)}
+		s.onmark=function(e){exc._support_.speakEvent(e, sc)}
+		s.onboundary=function(e){exc._support_.speakEvent(e, sc)}
+		s.onerror=function(e){exc._support_.speakEvent(e, sc)}
+		
+		if(stop) speechSynthesis.cancel()
+		speechSynthesis.speak(s)
+	} else if (stop) speechSynthesis.cancel()
+	else sc.error("execution",element,
+		new Error('cannot speak an empty string'))
 }
 
 SCxml.executableContentNS.tolerate.speak=exc.speak
